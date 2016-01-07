88bf48f3d562d2023def63704327659a64fe527d
Katja Hollar <afkatja@gmail.com>
2016-01-07 14:17:18 +0100

    setup for the matrix and an attempt to generate rows and columns

--- tictactoe.js
+++ tictactoe.js
@@ -1,17 +1,36 @@
+GameMatrix = new Mongo.Collection('matrix');
+
 if (Meteor.isClient) {
+  Meteor.startup(function(){
+    console.log('started up client');
+  });
   // counter starts at 0
   Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
+  Template.gameboard.helpers({
     counter: function () {
       return Session.get('counter');
+    },
+    boarditems: function(){
+      var matrix = [];
+      var size = 3; //3 boxes per row/col
+      var rows = [];
+      var cols = [];
+      //while (){}
+      return matrix;
     }
   });
 
-  Template.hello.events({
+  Template.gameboard.events({
     'click button': function () {
       // increment the counter when button is clicked
       Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+
+  Template.box.helpers({
+    index: function () {
+      return
     }
   });
 }
