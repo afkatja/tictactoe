8bdfbb290ffdc1d22df8af468120f14154a86148
Katja Hollar <afkatja@gmail.com>
2016-01-24 18:12:39 +0100

    Workshop tutorial - made the pages meteor based, with dynamic templates

---
+++ client/pages.js
@@ -0,0 +1,21 @@
+var routes = ['start', 'slides', 'step1', 'step2', 'step3', 'step4', 'step5', 'step6', 'step7'];
+var currentStep = new ReactiveVar('start');
+
+Template.mainnav.helpers({
+  routes: function(){
+    return routes;
+  }
+});
+
+Template.mainnav.events({
+  'click button': function () {
+    //`this` is the value of the dynamic template
+    currentStep.set(this);
+  }
+});
+
+Template.index.helpers({
+  tab: function () {
+    return currentStep.get();
+  }
+});
