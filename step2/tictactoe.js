c336cef2cfe57835df59552ab3076b7ba803d763
KR=10 <kristin@q42.nl>
2016-01-31 11:45:37 +0100

    updated step 1 and 2

--- step2/tictactoe.js
+++ step2/tictactoe.js
@@ -1,27 +1,31 @@
 Boxes = new Meteor.Collection('boxes');
 
 if (Meteor.isClient) {
-  Meteor.startup(function(){
-    console.log('started up client');
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
   });
 
-  Template.gameboard.helpers({
-    boxes: function(){
-      return Boxes.find({});
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
     }
   });
 }
 
 if (Meteor.isServer) {
-
-  // executed on startup of the server
   Meteor.startup(function () {
-    //just to be sure, we want to begin with a new (empty) collection
+    //just to be sure, we want to begin with a new collection
     Boxes.remove({});
     //and fill it with 9 boxes
     if(Boxes.find().count() === 0) {
       for(var i = 0; i < 9; i++){
-        Boxes.insert({boxIndex: i});
+        Boxes.insert({});
         console.log('inserted box with index', i);
       }
     }
