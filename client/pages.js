7e20f9a0af9f796878e88998640f2882de97d6c3
KR=10 <kristin@q42.nl>
2016-01-31 17:16:04 +0100

    added step 9 and 10 beginning

--- client/pages.js
+++ client/pages.js
@@ -1,5 +1,5 @@
-var routes = ['slides', 'start', 'step1', 'step2', 'step3', 'step4', 'step5', 'step6', 'step7', 'step8', 'step9'];
-var steps = ['step1', 'step2', 'step3', 'step4', 'step5', 'step6', 'step7', 'step8', 'step9'];
+var routes = ['slides', 'start', 'step1', 'step2', 'step3', 'step4', 'step5', 'step6', 'step7', 'step8', 'step9', 'step10'];
+var steps = ['step1', 'step2', 'step3', 'step4', 'step5', 'step6', 'step7', 'step8', 'step9', 'step10'];
 var currentStep = new ReactiveVar('start');
 
 UI.registerHelper('addOne', function(value) {
